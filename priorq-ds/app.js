460d011ff5c11b841b553e660337e7d26f8d2689
ayush raj <ayushthebest001@gmail.com>
2021-05-08 23:23:23 +0530

    pq

---
+++ priorq-ds/app.js
@@ -0,0 +1,51 @@
+
+  
+class PriorityQueue {
+  #list = [];
+  #capacity;
+
+  constructor(capacity) {
+    this.#capacity = Math.max(Number(capacity), 0) || null;
+  }
+
+  get size() {
+    return this.#list.length;
+  }
+
+  get isFull() {
+    return this.#capacity !== null && this.size === this.#capacity;
+  }
+
+  get isEmpty() {
+    return this.size === 0;
+  }
+
+  enqueue(item, priority = 0) {
+    priority = Math.max(Number(priority), 0);
+    const element = { item, priority };
+
+    if (
+      this.isEmpty ||
+      element.priority <= this.#list[this.size - 1].priority
+    ) {
+      this.#list.push(element);
+    } else {
+      for (let index in this.#list) {
+        if (element.priority > this.#list[index].priority) {
+          this.#list.splice(index, 0, element);
+          break;
+        }
+      }
+    }
+
+    return this.size;
+  }
+
+  dequeue() {
+    return this.isEmpty ? null : this.#list.shift().item;
+  }
+
+  toString() {
+    return this.#list.map((el) => el.item).toString();
+  }
+}
